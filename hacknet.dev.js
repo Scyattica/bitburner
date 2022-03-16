8ff2955cb3e41ce960bcc8501b059a2f1f950ff2
Scy <scyattica17@gmail.com>
2022-03-16 10:02:15 -0400

    why is this breaking everything

--- hacknet.dev.js
+++ hacknet.dev.js
@@ -1,31 +1,43 @@
 /** @param {NS} ns **/
 export async function main(ns) {
-	let nodes = ns.hacknet.numNodes();	
-    let hacknetnode = new HackNetNodes(0, ns.hacknet.getNodeStats(0).cores, ns.hacknet.getNodeStats(0).ram, ns.hacknet.getNodeStats(0).level)
-    let hacknetnode1 = new HackNetNodes(1, ns.hacknet.getNodeStats(1).cores, ns.hacknet.getNodeStats(1).ram, ns.hacknet.getNodeStats(1).level)
-    let object = {hacknetnode, hacknetnode1}
+    let serverArray = new Array()
 	//ns.tprint(Object.keys(object))
-    for(let key of Object.keys(object))
-    {
-        ns.tprint(object[key])
-    }
     //Todo:
     //Do a for loop and create a dictionary of the hacknetnodes class and do everything based off of that. 
-	//for(let i = 0; i < ns.hacknet.numNodes(); i++){ //Foreach node
-	//	BuyCores(ns, i)
-	//}
+	for(let i = 0; i < ns.hacknet.numNodes(); i++){ //Foreach node
+		serverArray.push( new HackNetNodes(i, ns.hacknet.getNodeStats(i).cores, ns.hacknet.getNodeStats(i).ram, ns.hacknet.getNodeStats(i).level))
+	}
+    //ns.tprint(serverArray)
+    serverArray.forEach(hsnode => {
+        buyCores(ns, hsnode);
+        buyRam(ns, hsnode);
+    })
 }
 
-function BuyCores(ns, hacknetindex){
+function buyCores(ns, hsnode){
 	let corestobuy = 0;
-	for(let j = ns.hacknet.getNodeStats(hacknetindex).cores; j < 16; j++) //Foreach Core that is needed? ? 
+	for(let j = ns.hacknet.getNodeStats(hsnode.index).cores; j < 16; j++) //Foreach Core that is needed? ? 
 	{
-		if(ns.hacknet.getCoreUpgradeCost(hacknetindex, corestobuy) < ns.getPlayer().money)//How many cores to buy
+		if(ns.hacknet.getCoreUpgradeCost(hsnode.index, corestobuy) < ns.getPlayer().money)//How many cores to buy
 		{
 			corestobuy++;
 		}
 	}
-	ns.tprint("Index:" + hacknetindex + ", Cores To Buy: " + corestobuy)
+	ns.tprint("Index:" + hsnode.index + ", Cores To Buy: " + corestobuy)
+    //ns.hacknet.upgradeCores(hsnode.index, corestobuy)
+}
+
+function buyRam(ns, hsnode){
+	let ramtobuy = 0;
+	for(let j = ns.hacknet.getNodeStats(hsnode.index).ram; j < 64; j * 2) //Foreach Core that is needed? ? 
+	{
+		if(ns.hacknet.getRamUpgradeCost(hsnode.index, ramtobuy) < ns.getPlayer().money)//How many rams to buy
+		{
+			ramtobuy++;
+		}
+	}
+	ns.tprint("Index:" + hsnode.index + ", Rams To Buy: " + ramtobuy)
+    //ns.hacknet.upgradeCores(hsnode.index, corestobuy)
 }
 
 class HackNetNodes {
