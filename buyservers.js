68116cb34fab0c2162a3ee043bc6b00923a73d53
Scyattica <scyattica17@gmail.com>
2022-04-24 21:22:23 -0400

    new buyserver logic. easy to make for loop?

--- buyservers.js
+++ buyservers.js
@@ -2,6 +2,44 @@
 export async function main(ns) {
     let prefix = ns.args[0]
     //ns.tprint(getMaxPurchaseRam(ns))
+    buyServer(ns, prefix)
+}
+
+function getMaxPurchaseRam(ns){
+    let j = 0;
+    let i = 0;
+    for(i = 1; j != 1; i = i * 2){
+        ns.print("Checking Purchase price with " + i + " ram")
+        if(ns.getPurchasedServerCost(i) > ns.getPlayer().money)
+        {
+            ns.print("Cost to high! I have " + ns.getPlayer().money + " but it costs " + ns.getPurchasedServerCost(i))
+            //ns.tprint(ns.getPurchasedServerMaxRam())
+            j = 1
+            break;
+        }
+        else
+        {
+            ns.print("Cost is good! I have " + ns.getPlayer().money + " and it costs " + ns.getPurchasedServerCost(i))
+            if(i >= ns.getPurchasedServerMaxRam())
+            {
+                ns.print("Max ram reached!")
+                j = 1;
+                return i
+                break;
+                
+            }
+            else
+            {
+                ns.print("Max Ram not reached...continuing!")
+            }
+        }
+    }
+    let newvalue = (i/2) // why is this a thing? because it still does a i * 2 even if it hits the thing apparently or breaks out of the loop? 
+    ns.tprint("newvalue is:" + newvalue + ", i is: " + i)
+    return newvalue
+}
+
+async function buyServer(ns, prefix){
     let servers = ns.getPurchasedServers()
     let newservername = ""
     if(prefix == null){
@@ -28,39 +66,7 @@ export async function main(ns) {
     if(prefix.toLowerCase() != "test"){
         
         ns.purchaseServer(newservername, maxram)
+        let files = ["runremote.js", "remotehack.js"]
+        ns.scp(files, "home", newservername)
     }
-}
-
-function getMaxPurchaseRam(ns){
-    let j = 0;
-    let i = 0;
-    for(i = 1; j != 1; i = i * 2){
-        ns.tprint("Checking Purchase price with " + i + " ram")
-        if(ns.getPurchasedServerCost(i) > ns.getPlayer().money)
-        {
-            ns.tprint("Cost to high! I have " + ns.getPlayer().money + " but it costs " + ns.getPurchasedServerCost(i))
-            //ns.tprint(ns.getPurchasedServerMaxRam())
-            j = 1
-            break;
-        }
-        else
-        {
-            ns.tprint("Cost is good! I have " + ns.getPlayer().money + " and it costs " + ns.getPurchasedServerCost(i))
-            if(i >= ns.getPurchasedServerMaxRam())
-            {
-                ns.tprint("Max ram reached!")
-                j = 1;
-                return i
-                break;
-                
-            }
-            else
-            {
-                ns.tprint("Max Ram not reached...continuing!")
-            }
-        }
-    }
-    let newvalue = (i/2) // why is this a thing? because it still does a i * 2 even if it hits the thing apparently or breaks out of the loop? 
-    ns.tprint("newvalue is:" + newvalue + ", i is: " + i)
-    return newvalue
 }
